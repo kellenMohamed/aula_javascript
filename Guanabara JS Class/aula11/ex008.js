54a538d690f5f6fa66981ceb0e122a5771c7903c
Fernandez <FernKE02@risk.regn.net>
2022-05-17 14:12:18 -0300

    Feat: inserted exercice 12 with interact js page

---
+++ Guanabara JS Class/aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log('Funcionou corretamente')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida e será MULTADO')
+}
