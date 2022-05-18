1c2a8b4f9bd696f2aa3d324714c4aa48d87fd36f
Fernandez <FernKE02@risk.regn.net>
2022-05-18 14:08:56 -0300

    Feat: done the exercise 13 and 14

---
+++ Guanabara JS Class/aula13/ambiente.js
@@ -0,0 +1,20 @@
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c} -> Tudo bem? -> While`)
+    c++
+}
+
+
+var d = 1
+do {    
+    console.log(`Passo ${d} -> Tudo bem? -> Do while`)
+    d++
+} while (d <= 6)
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
---
+++ Guanabara JS Class/aula14/ambiente.js
@@ -0,0 +1,16 @@
+var a = 1
+while (a<= 5){
+    console.log(`Passo:  ${a} - teste loop com While`)
+    a++
+}
+
+var b = 1
+do {
+    console.log(`Passo:  ${b} - teste loop com Do-While`)
+    b++
+} while (b < 5)
+
+
+for (var c=1;c<=10;c++){
+    console.log(`Passo:  ${c} - teste loop com For`)
+}
